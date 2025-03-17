399715a053b63411312b730ec0153407d89b19be
Pritam <pri10145@gmail.com>
2025-03-17 22:35:28 +0530

    analytics update

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -10,14 +10,14 @@ export function Analytics() {
       <Script
         id="google-analytics"
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-7K5YVZS9W4`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2QZC4DVDMN`}
       />
       <Script id="gtag-config" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-7K5YVZS9W4');
+          gtag('config', 'G-2QZC4DVDMN');
         `}
       </Script>
     </>
